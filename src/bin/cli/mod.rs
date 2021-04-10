c594549644337f55b1eb844363eb11be30999f05
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2021-04-10 17:12:31 +0300

    Update cli arguments description and operation

--- src/bin/cli/mod.rs
+++ src/bin/cli/mod.rs
@@ -1,19 +1,36 @@
 use clap::Clap;
 
+/// Manages operations for service database.
 #[derive(Clap)]
 #[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"))]
 pub struct Options {
     #[clap(subcommand)]
     pub command: Commands,
+    
+    /// URL to the Postgres SQL server host.
+    #[clap(short, long)]
+    pub host: String,
+
+    /// Listened port by Postgres SQL server.
+    #[clap(short, long)]
+    pub port: u16,
 }
 #[derive(Clap)]
 pub enum Commands {
+    /// Create initial database.
     Create(CreateOptions),
 }
 #[derive(Clap)]
 pub struct CreateOptions {
-    pub host: String,
+    /// A name of created database.
+    #[clap(long)]
     pub database_name: String,
+
+    /// A username of Postgress master accont. Password will be interactively requestd.
+    #[clap(long, default_value = "postgres")]
     pub master_user_name: String,
+
+    /// A username of created user to own the created database. Password will be interactively requested.
+    #[clap(long, default_value = "mcc-bot")]
     pub owner_user_name: String,
 }
--- src/bin/database-wizard.rs
+++ src/bin/database-wizard.rs
@@ -2,14 +2,23 @@ mod cli;
 
 use clap::Clap;
 use cli::{Commands, CreateOptions, Options};
+use sqlx::postgres;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let options = Options::parse();
 
     match options.command {
         Commands::Create(opts) => {
-            create_database(&opts);
+            create_database(
+                &options.host,
+                options.port,
+                &opts.master_user_name,
+                &opts.owner_user_name,
+                &opts.database_name,
+            )
+            .await;
         }
     }
 
@@ -21,7 +30,31 @@ fn request_password(message: &str) -> String {
     rpassword::read_password().expect("Failed to read a password from stdin!")
 }
 
-fn create_database(options: &CreateOptions) {
-    let master_password= request_password(&format!("Enter the master password for user name: {}", options.master_user_name));
-    let owner_password = request_password(&format!("Enter the new password for the user: {}", options.owner_user_name));
+async fn create_database(
+    host: &str,
+    port: u16,
+    master_user_name: &str,
+    owner_user_name: &str,
+    database_name: &str,
+) {
+    let master_password = request_password(&format!(
+        "Enter the master password for user name: {}",
+        master_user_name
+    ));
+    let owner_password = request_password(&format!(
+        "Enter the new password for the user: {}",
+        owner_user_name
+    ));
+
+    let connection_options = postgres::PgConnectOptions::new()
+        .host(host)
+        .username(master_user_name)
+        .password(&master_password)
+        .port(port)
+        .ssl_mode(postgres::PgSslMode::Prefer);
+
+    let connection = postgres::PgPoolOptions::new()
+        .connect_with(connection_options)
+        .await
+        .expect("Failed to connecto to the Postgrese server!");
 }
