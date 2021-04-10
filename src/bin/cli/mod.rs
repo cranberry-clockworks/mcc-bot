4280d9ade14617ba0b8a9603294c1cde7052d4a1
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2021-04-10 16:39:15 +0300

    Request password for db

--- src/bin/cli/mod.rs
+++ src/bin/cli/mod.rs
@@ -4,13 +4,16 @@ use clap::Clap;
 #[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"))]
 pub struct Options {
     #[clap(subcommand)]
-    command: Commands,
+    pub command: Commands,
 }
 #[derive(Clap)]
 pub enum Commands {
-    Create(CreateOptions)
+    Create(CreateOptions),
 }
 #[derive(Clap)]
 pub struct CreateOptions {
-    arg3: String
+    pub host: String,
+    pub database_name: String,
+    pub master_user_name: String,
+    pub owner_user_name: String,
 }
--- src/bin/database-wizard.rs
+++ src/bin/database-wizard.rs
@@ -1,7 +1,27 @@
-use clap::Clap;
-
 mod cli;
 
-fn main() {
-    let options = cli::Options::parse();
+use clap::Clap;
+use cli::{Commands, CreateOptions, Options};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let options = Options::parse();
+
+    match options.command {
+        Commands::Create(opts) => {
+            create_database(&opts);
+        }
+    }
+
+    Ok(())
+}
+
+fn request_password(message: &str) -> String {
+    println!("{}", message);
+    rpassword::read_password().expect("Failed to read a password from stdin!")
+}
+
+fn create_database(options: &CreateOptions) {
+    let master_password= request_password(&format!("Enter the master password for user name: {}", options.master_user_name));
+    let owner_password = request_password(&format!("Enter the new password for the user: {}", options.owner_user_name));
 }
