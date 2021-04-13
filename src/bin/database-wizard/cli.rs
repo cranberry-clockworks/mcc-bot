017eb9af8bda36bfebeff3500abfaf2ba4e6dcf2
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2021-04-13 09:57:53 +0300

    Add migration option to the wizard

--- src/bin/database-wizard/cli.rs
+++ src/bin/database-wizard/cli.rs
@@ -1,6 +1,6 @@
 use clap::Clap;
 
-/// Manages operations for service database.
+/// Manages operations on the service database.
 #[derive(Clap)]
 #[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"))]
 pub struct Options {
@@ -15,11 +15,16 @@ pub struct Options {
     #[clap(short, long, default_value = "5432")]
     pub port: u16,
 }
+
 #[derive(Clap)]
 pub enum Commands {
     /// Create initial database.
     Create(CreateOptions),
+
+    /// Migrate database to last available version.
+    Migrate(MigrateOptions),
 }
+
 #[derive(Clap)]
 pub struct CreateOptions {
     /// A name of created database.
@@ -28,9 +33,16 @@ pub struct CreateOptions {
 
     /// A username of Postgress master accont. Password will be interactively requestd.
     #[clap(long, default_value = "postgres")]
-    pub master_user_name: String,
+    pub master_username: String,
 
     /// A username of created user to own the created database. Password will be interactively requested.
     #[clap(long, default_value = "mcc")]
-    pub owner_user_name: String,
+    pub owner_username: String,
+}
+
+#[derive(Clap)]
+pub struct MigrateOptions {
+    /// Owner's username which manages service database.
+    #[clap(long, default_value = "mcc")]
+    pub owner_username: String,
 }
--- src/bin/database-wizard/main.rs
+++ src/bin/database-wizard/main.rs
@@ -7,51 +7,68 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let options = Options::parse();
+    let main_options = Options::parse();
 
-    match options.command {
-        Commands::Create(opts) => {
-            let master_password = request_password(&format!(
-                "Enter the master password for user name: {}",
-                opts.master_user_name
-            ));
-
-            let owner_password = request_password(&format!(
-                "Enter the new password for the user: {}",
-                opts.owner_user_name
-            ));
-
-            {
-                let connection = service::create_db_connection(
-                    &options.host,
-                    options.port,
-                    &opts.master_user_name,
-                    &master_password,
-                )
-                .await;
-                service::create_database(
-                    &connection,
-                    &opts.owner_user_name,
-                    &owner_password,
-                    &opts.database_name,
-                )
-                .await;
-            }
-
-            let connection = service::create_db_connection(
-                &options.host,
-                options.port,
-                &opts.owner_user_name,
-                &owner_password,
+    match main_options.command {
+        Commands::Create(command_options) => {
+            execute_create_command(
+                &main_options.host,
+                main_options.port,
+                &command_options.master_username,
+                &command_options.owner_username,
+                &command_options.database_name,
             )
             .await;
-            service::run_migrations(&connection).await;
+        }
 
-            println!("Finished!");
+        Commands::Migrate(command_options) => {
+            execute_migrate_command(
+                &main_options.host,
+                main_options.port,
+                &command_options.owner_username,
+            )
+            .await;
         }
     }
 
     Ok(())
+}
+
+async fn execute_create_command(
+    host: &str,
+    port: u16,
+    master_username: &str,
+    owner_username: &str,
+    database_name: &str,
+) {
+    let master_password = request_password(&format!(
+        "Enter the password for master username: {}",
+        master_username
+    ));
+
+    let owner_password = request_password(&format!(
+        "Enter the password for the new user: {}",
+        owner_username
+    ));
+
+    let connection =
+        service::create_db_connection(host, port, master_username, &master_password).await;
+
+    service::create_database(&connection, owner_username, &owner_password, database_name).await;
+
+    run_migration(host, port, owner_username, &owner_password).await;
+}
+
+async fn execute_migrate_command(host: &str, port: u16, username: &str) {
+    let password = request_password(&format!("Enter password for user: {}", username));
+
+    run_migration(host, port, username, &password).await;
+}
+
+async fn run_migration(host: &str, port: u16, username: &str, password: &str) {
+    let connection = service::create_db_connection(&host, port, &username, &password).await;
+
+    service::run_migrations(&connection).await;
 }
 
 fn request_password(message: &str) -> String {
