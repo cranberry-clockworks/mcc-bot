88a5c40d02f1fffb19d24d492bf6de636bf4df80
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2021-04-16 10:38:03 +0300

    Implement simple echo bot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-mod database;
+use teloxide::prelude::*;
+use teloxide::types::{KeyboardButton, KeyboardMarkup, ReplyMarkup};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+async fn run() {
+    teloxide::enable_logging!();
+    log::info!("Starting bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        let usr = message.update.from().unwrap();
+        let ans = format!(
+            "id:{}\nname:{}\ntext:{}",
+            usr.id,
+            usr.username.as_ref().unwrap_or(&String::new()),
+            &message.update.text().unwrap_or(&String::new())
+        );
+        let k = KeyboardMarkup::new(vec![
+            vec![KeyboardButton::new("A")],
+            vec![KeyboardButton::new("B"), KeyboardButton::new("C")],
+        ])
+        .one_time_keyboard(true);
+        let m = ReplyMarkup::Keyboard(k);
+        message.answer(&ans).reply_markup(m).await?;
+        respond(())
+    })
+    .await;
 }
