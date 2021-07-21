707af8c7caa30e805607d1491a69ad1702e26da5
Aleksei Abakumkin <cranberry.knight@gmail.com>
2021-07-21 19:19:14 +0300

    Reply on unrecognized command.

--- src/bot/respondents/default.rs
+++ src/bot/respondents/default.rs
@@ -5,12 +5,12 @@ use crate::localization;
 pub fn response(message: &str, frame: &mut Frame) -> Option<String> {
     let command = Command::try_parse(message);
     if command.is_none() {
-        return None;
+        return Some(localization::command_not_found_message());
     }
 
     match &command.unwrap().kind {
         CommandKind::Help => Some(generate_help_message()),
-        _ => None,
+        _ => Some(localization::command_not_found_message()),
     }
 }
 
--- src/localization/mod.rs
+++ src/localization/mod.rs
@@ -1,11 +1,20 @@
-use crate::bot::command;
 use crate::bot::command::CommandKind;
+
+pub fn default_command_list() -> String {
+    format!(
+        "Список команд:\n\
+        {0} - показать список команд",
+        CommandKind::Help
+    )
+}
 
 pub fn help_message() -> String {
     format!(
-        "Бот Московской Центральной Церкви\n\n\
-    Список комманд:\n
-    {0} — показать список команд.",
-        CommandKind::Help
+        "Бот Московской Центральной Церкви\n\n{}",
+        default_command_list()
     )
+}
+
+pub fn command_not_found_message() -> String {
+    format!("Неизвестная команда!\n\n{0}", default_command_list())
 }
